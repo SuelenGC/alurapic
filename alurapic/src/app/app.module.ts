b579d6c15ab660afc4a7bf57023e4df79f715679
SuelenGC <suelengcarvalho@gmail.com>
2021-04-23 03:30:23 -0300

    Move HttpClient Module to PhotosModule instead of AppModule

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,8 +11,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PhotosModule,
-    HttpClientModule
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
