e08364d2b6a7cca421ca8f5aca468e36cd375df5
SuelenGC <suelengcarvalho@gmail.com>
2021-04-23 03:21:02 -0300

    Majors including service

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { PhotoService } from './photos/photo/photo.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,18 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  photos = [
-    {
-      url: "https://www.rd.com/wp-content/uploads/2019/05/American-shorthair-cat-scaled.jpg",
-      description: "Cat 1"
-    },
-    {
-      url: "https://hips.hearstapps.com/clv.h-cdn.co/assets/15/33/1439400031-gettyimages-187144066.jpg",
-      description: "Cat 2"
-    },
-    {
-      url: "https://parade.com/wp-content/uploads/2020/12/Small-Cat-Breeds-1.jpg",
-      description: "Cat 3"
-    }
-  ]
+  photos: any[] = [];
+
+  constructor(photoService: PhotoService) {
+    photoService
+      .listFromUser('flavio')
+      .subscribe(photos => {
+        this.photos = photos
+      });
+  }
 }
