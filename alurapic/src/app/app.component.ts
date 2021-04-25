f1cfb2e8b9f6df1b4f4fb854955ea7a1e4b146e1
SuelenGC <suelengcarvalho@gmail.com>
2021-04-25 01:15:20 -0300

    Major changes

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { PhotoService } from './photos/photo/photo.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,14 +5,4 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
-export class AppComponent implements OnInit {
-  photos: any[] = [];
-
-  constructor(private photoService: PhotoService) {}
-
-  ngOnInit(): void {
-    this.photoService
-      .listFromUser('flavio')
-      .subscribe(photos => this.photos = photos);
-  }
-}
+export class AppComponent { }
--- alurapic/src/app/photos/photo/photo.service.ts
+++ alurapic/src/app/photos/photo/photo.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 import { Photo } from './photo';
+
+const API = 'http://localhost:3001';
 
 @Injectable({providedIn: 'root'})
 export class PhotoService {
@@ -8,6 +11,6 @@ export class PhotoService {
 
   listFromUser(userName: string) {
     return this.http
-      .get<Photo[]>('http://localhost:3001/photos');
+      .get<Photo[]>(API + '/user/' + userName + '/photos');
   }
 }
