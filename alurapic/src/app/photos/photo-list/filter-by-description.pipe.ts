31f7be1b15f35673801f2c17278512966920cc02
SuelenGC <suelengcarvalho@gmail.com>
2021-05-02 02:11:43 -0300

    A lot + image filter

--- alurapic/src/app/photos/photo-list/filter-by-description.pipe.ts
+++ alurapic/src/app/photos/photo-list/filter-by-description.pipe.ts
@@ -5,13 +5,13 @@ import { Photo } from '../photo/photo';
 @Pipe({ name: 'filterByDescription' })
 export class FilterByDescription implements PipeTransform {
   transform(photos: Photo[], descriptionQuery: string) {
-    descriptionQuery = descriptionQuery.trim().toLocaleLowerCase();
+    descriptionQuery = descriptionQuery.trim().toLowerCase();
 
     if (!descriptionQuery) {
       return photos;
     }
 
     return photos.filter(photo =>
-      photo.description.toLocaleLowerCase().includes(descriptionQuery));
+      photo.description.toLowerCase().includes(descriptionQuery));
   }
 }
